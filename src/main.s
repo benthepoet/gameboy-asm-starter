c37c2d8d8ed1b46887c7ca9c6b02def6b6a5d4ef
Ben Hanna <benpaulhanna@gmail.com>
2020-01-08 17:58:25 -0500

    Added sprite loading and v-blank interrupt.

--- src/main.s
+++ src/main.s
@@ -1,6 +1,11 @@
 INCLUDE "hardware.inc"
 
 
+SECTION "VBlank IRQ", ROM0[$40]
+
+VBlankIRQ:
+    ret
+
 SECTION "Header", ROM0[$100]
 
 EntryPoint:
@@ -17,9 +22,12 @@ SECTION "Game Code", ROM0[$150]
 Start:
 
 .waitVBlank
+
 	ld a, [rLY]
 	cp 144
 	jr c, .waitVBlank
+
+    ld sp, $fffe
 
 	; Disable display
 	xor a
@@ -28,6 +36,8 @@ Start:
 	; Set the palette
 	ld a, %11100100
 	ld [rBGP], a
+    ld [rOBP0], a
+    ld [rOBP1], a
 
 	; Reset scroll registers
 	xor a
@@ -40,9 +50,15 @@ Start:
     ; Load tiles
     ld de, FontTiles
     ld bc, FontTilesEnd - FontTiles
-    ld hl, $8800
+    ld hl, $8000
 
 	call MemCopy
+
+    ld de, FontTiles
+    ld bc, FontTilesEnd - FontTiles
+    ld hl, $8800
+
+    call MemCopy
 
     ; Load map
     ld hl, $9800
@@ -57,11 +73,33 @@ Start:
     ld a, b
     jr nz, .copyNumber
 
+    ; Load sprite
+    ld a, $20
+    ld [$FE00], a
+
+    ld a, $20
+    ld [$FE01], a
+
+    ld a, $02
+    ld [$FE02], a
+
+    ld a, %00000000
+    ld [$FE03], a
+
 	; Enable display with background
-	ld a, %10000001
+	ld a, %10000011
 	ld [rLCDC], a
 
+    ; Enable interrupts
+    ld a, %00000001
+    ld [rIE], a
+
+    ei
+    nop
+
 .loop
+    halt
+    nop
 	jr .loop
 
 
